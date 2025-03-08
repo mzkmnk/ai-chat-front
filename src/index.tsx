05a8316c076e8efa3f4fc5c8c66044700541b6fa
mzkmnk <sma0729mex@gmail.com>
2025-03-08 21:08:30 +0900

    refactor: add tailwind package and settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import { root } from "@lynx-js/react";
-
-import { App } from "./App.js";
 import { MemoryRouter, Route, Routes } from "react-router";
 import { SignIn } from "./pages/auth/SignIn.jsx";
+import { Chat } from "./pages/internal/chat.jsx";
 
+import "./App.css";
 root.render(
   <MemoryRouter>
     <Routes>
       <Route path="" element={<SignIn />} />
+      <Route path="internal/chat" element={<Chat />} />
     </Routes>
   </MemoryRouter>
 );
